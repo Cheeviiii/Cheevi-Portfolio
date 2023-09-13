b2b7495a8eca02005fd918b4a26a66757ce4ea0c
Cheeviiii <cheeviartz1@hotmail.com>
2023-09-13 13:45:47 -0400

    add contactform

--- src/components/ContatoForm/ContatoForm.tsx
+++ src/components/ContatoForm/ContatoForm.tsx
@@ -1,31 +1,37 @@
+"use client"
+
+import { ToastContainer } from "react-toastify";
+import sendEmail from "./function/sendEmail";
 import * as S from "./styles";
 
 export function ContatoForm() {
   return (
     <S.Container>
+      <ToastContainer />
       <S.Title>CONTATO</S.Title>
-      <S.Form>
+      <S.Form onSubmit={sendEmail}>
         <S.NomeEmail>
           <S.Label>
-            Nome <S.Input type="text" />
+            Nome <S.Input type="text" name="user_name" required />
           </S.Label>
 
           <S.Label>
             Email
-            <S.Input type="email" />
+            <S.Input type="email" name="user_email" required />
           </S.Label>
         </S.NomeEmail>
 
         <S.Sobre>
           <S.Label>
             Sobre
-            <S.Input type="text" />
+            <S.Input type="text" name="user_sobre" required />
           </S.Label>
         </S.Sobre>
 
         <S.Assunto>
-          <S.Label>Assunto
-            <S.Textarea />
+          <S.Label>
+            Assunto
+            <S.Textarea name="message" required />
           </S.Label>
         </S.Assunto>
 
---
+++ src/components/ContatoForm/function/sendEmail.ts
@@ -0,0 +1,32 @@
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+async function sendEmail(e: any) {
+  e.preventDefault();
+
+  try {
+    await emailjs.sendForm(
+      process.env.NEXT_PUBLIC_EMAILJS_SERVICE as string,
+      process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE as string,
+      e.target,
+      process.env.NEXT_PUBLIC_EMAILJS_KEY as string
+    ).then(() => {
+      toast.success("Email enviado com sucesso.", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    })
+    e.target.reset()
+  } catch (err) {
+    return console.log(err);
+  }
+};
+
+export default sendEmail;
